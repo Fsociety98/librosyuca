f22c303b21e79e72e7bce76e3cf30e7be7e8c960
Fsociety98 <alejandro98hh@gmail.com>
2020-06-01 02:11:43 -0500

    migraciones

---
+++ app/Http/Controllers/UsuarioLibroController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UsuarioLibro;
+use Illuminate\Http\Request;
+
+class UsuarioLibroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UsuarioLibro  $usuarioLibro
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UsuarioLibro $usuarioLibro)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\UsuarioLibro  $usuarioLibro
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UsuarioLibro $usuarioLibro)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UsuarioLibro  $usuarioLibro
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UsuarioLibro $usuarioLibro)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UsuarioLibro  $usuarioLibro
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UsuarioLibro $usuarioLibro)
+    {
+        //
+    }
+}
